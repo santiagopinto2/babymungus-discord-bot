fe7abf02e8faaa17cc4d4b57d607658d05fa69fc
Santiago Pinto <fablessb@gmail.com>
2020-10-04 16:34:21 -0400

    added onto help command

--- commands/help.js
+++ commands/help.js
@@ -16,6 +16,7 @@ module.exports = {
                 { name: '!unmute', value: 'Unmutes all members in the voice channel the user is in ' },
                 { name: '!togglemute', value: 'Adds a reaction to the !togglemute command just sent that the user can react to in order to mute or unmute all members in the voice channel the user is in' },
                 { name: '\u200B', value: '\u200B' },
+                { name: 'Link to invite BabyMungus to your server', value: 'https://bit.ly/3iuwwGI' },
                 { name: 'If you find any bugs or have any suggestions please feel free to join the Discord server at', value: 'https://discord.gg/NkQCE68' },
                 { name: 'If you would like to help support the continuation of the bot you can visit', value: 'https://paypal.me/santiagopinto1 or search for @Santiago-Pinto on Venmo' },
             )
